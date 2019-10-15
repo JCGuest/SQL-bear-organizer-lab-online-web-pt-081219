b723a4a6c82aa9000f6f5d4a7ef4f63fa8734e7e
John Guest <jcguest90@gmail.com>
2019-10-15 03:24:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears 
 (name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+VALUES ('NULL', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
 (name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
