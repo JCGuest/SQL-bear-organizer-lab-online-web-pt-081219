5ea34d01569aeed363d52c423e92ace3fa0daef4
John Guest <jcguest90@gmail.com>
2019-10-15 03:23:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,25 +1,25 @@
 INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3 , "Scottish Fold", 1);
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive);
+(name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
