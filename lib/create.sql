e0225866091bd6ff68b17b3b31371e9c21d7578b
John Guest <jcguest90@gmail.com>
2019-10-15 03:14:03 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,2 +1,2 @@
 CREATE TABLE bears (
-  name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive TEXT);
+  id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive TEXT);
