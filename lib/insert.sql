c512be3056623e349369c58a3ad50dab22058e69
John Guest <jcguest90@gmail.com>
2019-10-15 03:37:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,24 +1,3 @@
 INSERT INTO bears 
 (name, age, gender, gender, color, temperament, alive)
 VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
-INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
-INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
-INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
-INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
-INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
-INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
-INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
