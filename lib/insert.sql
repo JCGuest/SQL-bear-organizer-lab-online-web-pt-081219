a8d370ef686595a0e89a5bfa16e604f3ce2b63db
John Guest <jcguest90@gmail.com>
2019-10-15 03:36:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears 
-(name, age, gender, color, temperament, alive)
-VALUES (NULL, '0', 'gender', 'color', 'temperament', 'alive');
+(name, age, gender, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 INSERT INTO bears 
 (name, age, gender, color, temperament, alive)
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
