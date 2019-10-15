f5fbe855894232c2c9407b504f8a05c618d0754f
John Guest <jcguest90@gmail.com>
2019-10-15 03:18:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,24 @@
 INSERT INTO bears 
 (name, age, gender, color, temperament, alive);
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive);
+VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive);
+VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive);
+VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive);
+VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive);
+VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive);
+VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
+INSERT INTO bears 
+(name, age, gender, color, temperament, alive);
+VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
