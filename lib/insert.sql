41296595936296a4e9a8c259f24a8220de1d407d
John Guest <jcguest90@gmail.com>
2019-10-15 03:22:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
+INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3 , "Scottish Fold", 1);
 INSERT INTO bears 
 (name, age, gender, color, temperament, alive);
 VALUES ('name', '0', 'gender', 'color', 'temperament', 'alive');
